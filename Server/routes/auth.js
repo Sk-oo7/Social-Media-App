8f638bf6806fbda5706a7c10c061d32fb0166d44
Shubham Khullar <shubham5khullar@gmail.com>
2020-10-19 23:02:28 +0530

    User authorization
    Checking token for user before giving access of private data.

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -5,6 +5,12 @@ const User = mongoose.model("User")
 const bcrypt =require('bcryptjs')
 const jwt =require('jsonwebtoken')
 const {JWT_SECRET}=require('../keys')
+const requireLogin =require("../middleware/requireLogin")
+
+
+router.get("/protected",requireLogin,(req,res)=>{
+    res.send("hello user")
+})
 
 router.post("/signup",(req,res)=>{
     const {name,email,password}=req.body
@@ -65,4 +71,5 @@ router.post("/signin",(req,res)=>{
         })
     })
 })
+
 module.exports=router
