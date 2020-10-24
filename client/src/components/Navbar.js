042267c972678e1c1162d78ed7572de4c616c235
Shubham Khullar <shubham5khullar@gmail.com>
2020-10-24 15:41:45 +0530

    Updated Profile Page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "../App.css"
 
-function navbar() {
+function Navbar() {
+  const [show,setShow]=useState(false);
+
     return (
         <div>
             <nav>
-    <div className="nav-wrapper dark blue darken-3">
-      <Link to="/" className="brand-logo">Fleet</Link>
-      <ul id="" className="right hide-on-med-and-down">
-        <li><Link to="/signin">SignIn</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-      </ul>
-    </div>
-  </nav>
+              <div className="nav-wrapper dark blue darken-3">
+                <Link to="/" className="brand-logo center">Fleet</Link>
+                <Link className='sidenav-trigger right' to="#" data-target='' onClick={()=>setShow(!show)}><i className="material-icons">menu</i></Link>
+                {!show && <ul id="nav-mobile" className="right hide-on-med-and-down">
+                  <li><Link to="/signin">SignIn</Link></li>
+                  <li><Link to="/signup">Signup</Link></li>
+                  <li><Link to="/profile">Profile</Link></li>
+                </ul>}
+              </div>
+            </nav>
+
+            {show && <nav>
+              <div className="nav-wrapper white" style={{display:"flex",justifyContent:"space-around"}}>
+                <ul className="show-on-medium-and-down">
+                <li><Link to="/signin"><span className="blue-text text-darken-2">SignIn</span></Link></li>
+                <li><Link to="/signup"><span className="blue-text text-darken-2">SignUp</span></Link></li>
+                <li><Link to="/profile"><span className="blue-text text-darken-2">Profile</span></Link></li>
+                </ul>
+              </div>
+            </nav>}
         </div>
     )
 }
 
-export default navbar
+export default Navbar
--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -2,8 +2,34 @@ import React from 'react'
 
 function Profile() {
     return (
-        <div>
-            profile
+        <div style={{maxWidth:"80%", margin:"0 auto"}}>
+            <div style={{display:"flex", justifyContent:"space-around", margin: "18px 0px",flexWrap:"wrap"
+            // ,borderBottom:"1px solid grey"
+            }}>
+                <div><img style={{width:"160px", height:"160px", borderRadius:"80px", objectFit: "cover"}} 
+                src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                alt="" /></div>
+                <div>
+                    <h4>Muskan</h4>
+                    <div style={{display:"flex",justifyContent:"space-between"}}>
+                    <div><h6>10 posts &nbsp;</h6></div>
+                    <div><h6>100 followers &nbsp;</h6></div> 
+                    <div><h6>102 following &nbsp;</h6></div> 
+                    </div>
+                </div>
+                
+            </div>
+            <hr/>
+            <div className="gallery" style={{marginTop:"20px"}}>
+                <img className="item" src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                <img className="item" src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                <img className="item" src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                <img className="item" src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                <img className="item" src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                <img className="item" src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                <img className="item" src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+                <img className="item" src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+            </div>
         </div>
     )
 }
