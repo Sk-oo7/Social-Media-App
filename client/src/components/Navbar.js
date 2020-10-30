797ae532627a17647118fc70f6a210cc1b7bb741
Shubham Khullar <shubham5khullar@gmail.com>
2020-10-30 23:44:31 +0530

    Update Navbar.js
    Applied conditions for links on navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState,useContext } from 'react'
 import { Link } from 'react-router-dom'
+import {UserContext} from "../App"
 import "../App.css"
 
 function Navbar() {
+  const {state,dispatch} = useContext(UserContext)
+
+  const renderList =()=>{
+    if(state){
+      return [
+        <li><Link to="/profile">Profile</Link></li>
+      ]
+    }
+    else{
+      return [<li><Link to="/signin">SignIn</Link></li>,
+      <li><Link to="/signup">Signup</Link></li>]
+    }
+  }
+  const renderList2 =()=>{
+    if(state){
+      return [
+        <li><Link to="/profile"><span className="blue-text text-darken-2">Profile</span></Link></li>
+      ]
+    }
+    else{
+      return [
+        <li><Link to="/signin"><span className="blue-text text-darken-2">SignIn</span></Link></li>,
+        <li><Link to="/signup"><span className="blue-text text-darken-2">SignUp</span></Link></li>
+    ]
+    }
+  }
   const [show,setShow]=useState(false);
 
     return (
         <div>
             <nav>
               <div className="nav-wrapper dark blue darken-3">
-                <Link to="/" className="brand-logo center">Fleet</Link>
+                <Link to={state? "/" : "/signin"} className="brand-logo center">Fleet</Link>
                 <Link className='sidenav-trigger right' to="#" data-target='' onClick={()=>setShow(!show)}><i className="material-icons">menu</i></Link>
                 {!show && <ul id="nav-mobile" className="right hide-on-med-and-down">
-                  <li><Link to="/signin">SignIn</Link></li>
-                  <li><Link to="/signup">Signup</Link></li>
-                  <li><Link to="/profile">Profile</Link></li>
+                  {renderList()}
                 </ul>}
               </div>
             </nav>
@@ -22,9 +47,7 @@ function Navbar() {
             {show && <nav>
               <div className="nav-wrapper white" style={{display:"flex",justifyContent:"space-around"}}>
                 <ul className="show-on-medium-and-down">
-                <li><Link to="/signin"><span className="blue-text text-darken-2">SignIn</span></Link></li>
-                <li><Link to="/signup"><span className="blue-text text-darken-2">SignUp</span></Link></li>
-                <li><Link to="/profile"><span className="blue-text text-darken-2">Profile</span></Link></li>
+                {renderList2()}
                 </ul>
               </div>
             </nav>}
