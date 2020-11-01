2cff987cfb9e2761aa9b26b7b6016926cf1b45d5
Shubham Khullar <shubham5khullar@gmail.com>
2020-11-02 00:11:16 +0530

    Implemented Like and unlike functionality
    Now users can like and unlike posts

--- Server/routes/post.js
+++ Server/routes/post.js
@@ -48,26 +48,26 @@ router.get("/mypost",requireLogin,(req,res)=>{
 
 router.put("/like",requireLogin,(req,res)=>{
     Post.findByIdAndUpdate(req.body.postId,{
-        $push:{likes:req.user._Id}
+        $push:{likes:req.user._id}
     },{
         new:true
     }).exec((err,result)=>{
         if(err){
             return res.status(422).json({error:err})
-        } else{
+        }else{
             res.json(result)
         }
     })
 })
 router.put("/unlike",requireLogin,(req,res)=>{
     Post.findByIdAndUpdate(req.body.postId,{
-        $pull:{likes:req.user._Id}
+        $pull:{likes:req.user._id}
     },{
         new:true
     }).exec((err,result)=>{
         if(err){
             return res.status(422).json({error:err})
-        } else{
+        }else{
             res.json(result)
         }
     })
--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import CreatePost from"./CreatePost"
+import {UserContext} from "../../App"
 
 function Home() {
     const [data,setData] = useState([]);
+    const{state,dispatch} = useContext(UserContext)
     useEffect(()=>{
         fetch("/allpost",{
             headers:{
@@ -10,9 +12,59 @@ function Home() {
             }
         }).then(res=> res.json())
         .then(result=>{
+            console.log(result)
             setData(result.posts)
         })
     },[])
+
+    const likePost = (id)=>{
+        fetch("/like",{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            const newData = data.map(item=>{
+                if(item._id == result._id){
+                    return result
+                }
+                else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+    const unlikePost = (id)=>{
+        fetch("/unlike",{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            const newData = data.map(item=>{
+                if(item._id == result._id)
+                    return result
+                else
+                    return item
+            })
+            setData(newData)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
     return (
         <div className="home">
             <CreatePost />
@@ -25,7 +77,8 @@ function Home() {
                                 <img alt="" src={item.photo} />
                             </div>
                             <div className="card-content">
-                                <i className="material-icons" style={{color:"red"}}>favorite</i>
+                                {item.likes.includes(state._id)?<i className="material-icons" style={{color:"red"}} onClick={()=>{unlikePost(item._id)}} >favorite</i> : <i className="material-icons" style={{color:"red"}} onClick={()=>{likePost(item._id)}} >favorite_border</i>}
+                                <h6>{item.likes?.length} likes</h6>
                                 <h6>{item.title}</h6>
                                 <p>{item.body}</p>
                                 <input type="text" placeholder="Add Comment" />
