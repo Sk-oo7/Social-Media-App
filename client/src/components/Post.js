a681adc9fffb374632c39e39b07be696bffc19aa
Shubham Khullar <shubham5khullar@gmail.com>
2020-11-03 15:12:58 +0530

    Added Loading icon
    Loader is added and fixed some bugs

---
+++ client/src/components/Post.js
@@ -0,0 +1,152 @@
+import React, { useEffect, useState, useContext } from 'react'
+import {UserContext} from "../App"
+import { WaveLoading  } from 'react-loadingg';
+
+function Post(item) {
+
+    const [lcount,setLcount]=useState(item.item.likes?.length);
+    const [showload,setShowload]=useState(false);
+    const{state} = useContext(UserContext)
+    const [show,setShow]=useState(item.item.likes.some(likes=>likes.postedBy == state._id))
+
+    const [data,setData] = useState([]);
+    useEffect(()=>{
+        fetch("/allpost",{
+            headers:{
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            }
+        }).then(res=> res.json())
+        .then(result=>{
+            setData(result.posts)
+        })
+    },[])
+
+     useEffect(() => {
+        let value = localStorage.getItem("sidebar-scroll");
+        setShowload(true)
+        setTimeout(() => {
+            setShowload(false)
+            if (value !== null) {
+                window.scrollTo(0,value);
+                localStorage.setItem("sidebar-scroll", null);
+            }
+          }, 500); 
+    },[])
+
+    const likePost = (id)=>{
+        setLcount(lcount+1)
+        setShow(!show)
+        fetch("/like",{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            const newData = data.map(item=>{
+                if(item._id == result._id){
+                    return result
+                }
+                else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+    const unlikePost = (id)=>{
+        setLcount(lcount-1)
+        setShow(!show)
+        fetch("/unlike",{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            const newData = data.map(item=>{
+                if(item._id == result._id)
+                    return result
+                else
+                    return item
+            })
+            setData(newData)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+    const makeComment = (text,postId)=>{
+        fetch('/comment',{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId,
+                text
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            console.log(result)
+            const newData = data.map(item=>{
+              if(item._id==result._id){
+                  return result
+              }else{
+                  return item
+              }
+           })
+          setData(newData)
+        }).catch(err=>{
+            console.log(err)
+        })
+        localStorage.setItem("sidebar-scroll", window.scrollY);
+        window.location.reload(false);
+  }
+
+    if(showload)
+        return <WaveLoading color="#1e88e5" speed={1} />
+    else return (
+        <div className="card home-card" key={item.item._id}>
+            
+                            <h5>{item.item.postedBy.name}</h5>
+                            <div className="card-image">
+                                <img alt="" src={item.item.photo} />
+                            </div>
+                            <div className="card-content">
+                                
+                                {show?  <i className="btn-flat btn-small btn-floating center material-icons N/A transparent" style={{color:"red"}} onClick={()=>{unlikePost(item.item._id)}} >favorite</i> :
+                                <i className="btn-flat btn-small btn-floating center material-icons N/A transparent" style={{color:"red"}} onClick={()=>{likePost(item.item._id)}} >favorite_border</i>}
+                                <span style={{fontSize:"22px"}}>{lcount} likes</span>
+                                <h6>{item.item.title}</h6>
+                                <p>{item.item.body}</p>
+                                {
+                                    item.item.comments.map(record=>{
+                                        return <h6 key={record._id}> 
+                                            <b>{record.postedBy.name} </b>
+                                            <span>{record.text}</span>
+                                        </h6>
+                                    })
+                                }
+                                <form onSubmit={(e)=>{
+                                    e.preventDefault()
+                                    makeComment(e.target[0].value,item.item._id)
+                                }}>
+                                    <input className="input-field" type="text" placeholder="Add Comment" />
+                                </form>
+                            </div>
+                        </div>
+    )
+}
+
+export default Post
--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState, useContext } from 'react'
 import CreatePost from"./CreatePost"
-import {UserContext} from "../../App"
 import "../../App.css"
+import Post from '../Post';
 
 function Home() {
 
     const [data,setData] = useState([]);
-    const{state} = useContext(UserContext)
+
     useEffect(()=>{
         fetch("/allpost",{
             headers:{
@@ -19,133 +19,14 @@ function Home() {
         })
     },[])
 
-    useEffect(() => {
-        let value = localStorage.getItem("sidebar-scroll");
-        setTimeout(() => {
-            if (value !== null) {
-                window.scrollTo(0,value);
-            }
-          }, 100);
-    })
-
-    const likePost = (id)=>{
-        fetch("/like",{
-            method:"put",
-            headers:{
-                "Content-Type":"application/json",
-                "Authorization":"Bearer "+localStorage.getItem("jwt")
-            },
-            body:JSON.stringify({
-                postId:id
-            })
-        }).then(res=>res.json())
-        .then(result=>{
-            const newData = data.map(item=>{
-                if(item._id == result._id){
-                    return result
-                }
-                else{
-                    return item
-                }
-            })
-            setData(newData)
-        }).catch(err=>{
-            console.log(err)
-        })
-        
-            localStorage.setItem("sidebar-scroll", window.scrollY);
-          
-        window.location.reload(false);
-    }
-    const unlikePost = (id)=>{
-        fetch("/unlike",{
-            method:"put",
-            headers:{
-                "Content-Type":"application/json",
-                "Authorization":"Bearer "+localStorage.getItem("jwt")
-            },
-            body:JSON.stringify({
-                postId:id
-            })
-        }).then(res=>res.json())
-        .then(result=>{
-            const newData = data.map(item=>{
-                if(item._id == result._id)
-                    return result
-                else
-                    return item
-            })
-            setData(newData)
-        }).catch(err=>{
-            console.log(err)
-        })
-        localStorage.setItem("sidebar-scroll", window.scrollY);
-        window.location.reload(false);
-    }
-    const makeComment = (text,postId)=>{
-        fetch('/comment',{
-            method:"put",
-            headers:{
-                "Content-Type":"application/json",
-                "Authorization":"Bearer "+localStorage.getItem("jwt")
-            },
-            body:JSON.stringify({
-                postId,
-                text
-            })
-        }).then(res=>res.json())
-        .then(result=>{
-            console.log(result)
-            const newData = data.map(item=>{
-              if(item._id==result._id){
-                  return result
-              }else{
-                  return item
-              }
-           })
-          setData(newData)
-        }).catch(err=>{
-            console.log(err)
-        })
-  }
-
     return (
         <div className="home">
             <CreatePost />
            
             {
-                data?.map(item=>{
-                    return(
-                        <div className="card home-card" key={item._id}>
-                            <h5>{item.postedBy.name}</h5>
-                            <div className="card-image">
-                                <img alt="" src={item.photo} />
-                            </div>
-                            <div className="card-content">
-                                {item.likes.some(likes=>likes.postedBy == state._id)?
-                                <i className="btn-flat btn-small btn-floating center material-icons N/A transparent" style={{color:"red"}} onClick={()=>{unlikePost(item._id)}} >favorite</i> : 
-                                <i className="btn-flat btn-small btn-floating center material-icons N/A transparent" style={{color:"red"}} onClick={()=>{likePost(item._id)}} >favorite_border</i>}
-                                <span style={{fontSize:"22px"}}>{item.likes?.length} likes</span>
-                                <h6>{item.title}</h6>
-                                <p>{item.body}</p>
-                                {
-                                    item.comments.map(record=>{
-                                        return <h6 key={record._id}> 
-                                            <b>{record.postedBy.name} </b>
-                                            <span>{record.text}</span>
-                                        </h6>
-                                    })
-                                }
-                                <form onSubmit={(e)=>{
-                                    e.preventDefault()
-                                    makeComment(e.target[0].value,item._id)
-                                }}>
-                                    <input className="input-field" type="text" placeholder="Add Comment" />
-                                </form>
-                            </div>
-                        </div>
-                    )
-                })
+                data?.map(item=>
+                    <Post item={item} />
+                )
             }
             
             
