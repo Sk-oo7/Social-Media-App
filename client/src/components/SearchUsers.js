462821a2ac03897af495d17b06db5d902164fa63
Shubham Khullar <shubham5khullar@gmail.com>
2020-11-14 20:54:45 +0530

    Update SearchUsers.js

--- client/src/components/SearchUsers.js
+++ client/src/components/SearchUsers.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Avatar from "@material-ui/core/Avatar"
+import {UserContext} from "../App"
+import { Link } from 'react-router-dom';
 
 function SearchUsers() {
     const [search,setSearch] = useState("");
     const [result,setResult] = useState([]);
+    const{state} = useContext(UserContext)
 
     const fetchUsers = (query)=>{
         setSearch(query)
@@ -22,13 +25,18 @@ function SearchUsers() {
     }
 
     return (
-        <div style={{margin:"10px auto", maxWidth:"30%", minWidth:"200px", padding:"20px"}}>
+        <div style={{margin:"10px auto", maxWidth:"30%", minWidth:"200px"}}>
             <input type='text' placeholder="Search users" value={search} onChange={(e)=>fetchUsers(e.target.value)} />
          
             {search && 
-                <ul class="collection">
+                <ul className="collection">
                     {result?.map(item=>
-                        <li class="collection-item" style={{display:"flex"}}><Avatar src={item.pic} /> <h5>{item.name}</h5></li>
+                        item._id !== state._id? <Link to={"/profile/"+item._id}><li key={item._id} className="collection-item" style={{display:"flex",paddingLeft:"10px"}}>
+                            <Avatar src={item.pic} /> <div style={{display:"flex",flexDirection:"column"}}><h6 style={{marginLeft:"5px",marginTop:"8px",fontSize:"20px"}}>{item.name}</h6><small style={{color:"gray",marginTop:"-10px"}}>({item.email})</small></div>
+                        </li></Link>:
+                        <Link to="/profile"><li key={item._id} className="collection-item" style={{display:"flex",paddingLeft:"10px"}}>
+                        <Avatar src={item.pic} /> <div style={{display:"flex",flexDirection:"column"}}><h6 style={{marginLeft:"5px",marginTop:"8px",fontSize:"20px"}}>{item.name}</h6><small style={{color:"gray",marginTop:"-10px"}}>({item.email})</small></div>
+                    </li></Link>
                     )}
                 </ul>
             }
